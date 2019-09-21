ecc1617553303c5982cc4aaca39f63e3ae1b7a1b
nishkaush <kaushish.nishant@hotmail.com>
2019-09-21 10:43:03 +1000

    added unshift method to singlylinked list

--- data-structures/singly-linked-list.js
+++ data-structures/singly-linked-list.js
@@ -13,7 +13,7 @@ class SinglyLinkedList {
   }
 
   push(val) {
-    //create a new node with a given value
+    //adds an item to the end of the list
     let newNode = new Node(val);
 
     if (this.length === 0) {
@@ -30,7 +30,6 @@ class SinglyLinkedList {
   }
 
   pop() {
-    console.log("running pop");
     //removes last item in the linked list
     if (this.length === 0) return undefined;
     let current = this.head;
@@ -51,13 +50,22 @@ class SinglyLinkedList {
   }
 
   shift() {
-    //get this.head and save it
+    //removes an item from start of the list
     if (this.length === 0) return undefined;
     let itemToShift = this.head.val;
     this.head = this.head.next;
     this.length -= 1;
     if (this.length === 0) this.tail = null;
     return itemToShift;
+  }
+
+  unshift(val) {
+    //adds an item to the start of the list
+    if (this.length === 0) return this.push(val);
+    let currentHead = this.head;
+    this.head = { val, next: currentHead };
+    this.length += 1;
+    return this;
   }
 }
 
@@ -67,4 +75,4 @@ list.push("Max");
 list.push("Whats");
 list.push("Happening!");
 
-setTimeout(() => console.log(list, shifted), 1000);
+setTimeout(() => console.log(list), 1000);
